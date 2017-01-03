beb5f535e61f14da24ddf8f1647d6e2d82c44354
Elisey <elisey.rav@gmail.com>
2017-01-04 02:55:41 +0700

    Add proper command line parser

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import DataBase
 from prettytable import PrettyTable
+import argparse
+
 
 def searchCommand(args):
     if args[0] == '-s':
@@ -59,10 +61,57 @@ def searchCommand(args):
 def main():
     while True:
         args = input('>').split()
-        print(args)
-        if args[0] == 'search':
-            searchCommand(args[1:])
+        #print(args)
 
+        parser = argparse.ArgumentParser(prog='PROG')
+        #parser.add_argument('--foo', action='store_true', help='foo help')
+        subparsers = parser.add_subparsers(help='sub-command help')
+
+        #PARSER SEARCH
+        parser_search = subparsers.add_parser('search', help='search help')
+        parser_search.add_argument('search', action='store_true')
+        parser_search.add_argument('--name', '-n', dest='name', type=str, help='name help')
+
+        #PARSER STORAGE
+        parser_storage = subparsers.add_parser('storage', help='storage help')
+        parser_storage.add_argument('storage', action='store_true')
+        parser_storage.add_argument('--add', '-a', action='store_true')
+        parser_storage.add_argument('--view', '-v', action='store_true')
+
+        parser_storage.add_argument('--name', '-n', dest='name', type=str)
+        parser_storage.add_argument('--parentid', '-p', dest='parent', type=int)
+        parser_storage.add_argument('--id', '-i', dest='id', type=int)
+
+        #PARSER ITEM
+        parser_item = subparsers.add_parser('item', help='item help')
+        parser_item.add_argument('item', action='store_true')
+        parser_item.add_argument('--view', '-v', action='store_true')
+        parser_item.add_argument('--create', '-c', action='store_true')
+        parser_item.add_argument('--add', '-a', action='store_true')
+        parser_item.add_argument('--remove', '-r', action='store_true')
+        parser_item.add_argument('--move', '-m', action='store_true')
+
+        parser_item.add_argument('--name', '-n', dest='name', type=str)
+        parser_item.add_argument('--id', '-i', dest='id', type=int)
+        parser_item.add_argument('--storagename', dest='storagename', type=str)
+        parser_item.add_argument('--storageid', dest='storageid', type=int)
+        parser_item.add_argument('--storagenamesrc', dest='storagenamesrc', type=str)
+        parser_item.add_argument('--storageidsrc', dest='storageidsrc', type=int)
+        parser_item.add_argument('--storagenamedst', dest='storagenamedst', type=str)
+        parser_item.add_argument('--storageiddst', dest='storageiddst', type=int)
+        parser_item.add_argument('--quantity', '-q', dest='quantity', type=int)
+
+        #PARSER TREE
+        parser_tree = subparsers.add_parser('tree', help='tree help')
+        parser_tree.add_argument('tree', action='store_true')
+        parser_tree.add_argument('--name', '-n', dest='name', type=str)
+        parser_tree.add_argument('--id', '-i', dest='id', type=int)
+
+        #PARSER STATUS
+        parser_status = subparsers.add_parser('status', help='status help')
+        parser_status.add_argument('status', action='store_true')
+
+        print(parser.parse_args(args))
 
 
 if __name__ == '__main__':
