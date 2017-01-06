b59f3b62e3aeec67f2ffda4acf763384f021b938
Elisey <elisey.rav@gmail.com>
2017-01-06 19:13:49 +0700

    Main functions for adding, moving and removing items

--- main.py
+++ main.py
@@ -70,7 +70,72 @@ def itemCreate(itemName):
         print("Ошибка. Элемент ", itemName, "не добавлен в базу. Возможно элемент с таким именем уже существует")
 
 def itemAdd(itemName = None, itemId = None, storageName = None, storageId = None, quantity = 0):
-    print("Adding new item")
+    if quantity == 0:
+        print("Ошибка при добавлении элемента. Количество элементов должно быть больше ноля")
+        return
+
+    if itemId is None:
+        itemId, result = DataBase.getItemIdByName(itemName)
+        if result is False:
+            print("Неизвестная ошибка при добавлении элемента")
+            return
+
+    if storageId is None:
+        storageId, result = DataBase.getStorageIdByName(storageName)
+        if result is False:
+            print("Неизвестная ошибка при добавлении элемента")
+            return
+    DataBase.addItems(itemId, storageId, quantity)
+    print("Элементы успешно добавлены")
+
+def itemRemove(itemName = None, itemId = None, storageName = None, storageId = None, quantity = 0):
+    if itemId is None:
+        itemId, result = DataBase.getItemIdByName(itemName)
+        if result is False:
+            print("Неизвестная ошибка при удалении элемента")
+            return
+
+    if storageId is None:
+        storageId, result = DataBase.getStorageIdByName(storageName)
+        if result is False:
+            print("Неизвестная ошибка при удалении элемента")
+            return
+    result = DataBase.removeItems(itemId, storageId, quantity)
+
+    if result == False:
+        print("Ошибка при удалении элемента. Возможно указано некорректное количество элементов для удаления")
+        return
+
+    print("Элементы успешно удалены")
+    return
+
+def itemMove(itemName = None, itemId = None, storageNameSrc = None, storageIdSrc = None, storageNameDst = None, storageIdDst = None, quantity = 0):
+    if itemId is None:
+        itemId, result = DataBase.getItemIdByName(itemName)
+        if result is False:
+            print("Неизвестная ошибка при удалении элемента")
+            return
+
+    if storageIdSrc is None:
+        storageIdSrc, result = DataBase.getStorageIdByName(storageNameSrc)
+        if result is False:
+            print("Неизвестная ошибка при удалении элемента")
+            return
+
+    if storageIdDst is None:
+        storageIdDst, result = DataBase.getStorageIdByName(storageNameDst)
+        if result is False:
+            print("Неизвестная ошибка при удалении элемента")
+            return
+
+    result = DataBase.moveItems(itemId, storageIdSrc, storageIdDst, quantity)
+
+    if result == False:
+        print("Ошибка при перемещении элемента. Возможно указано некорректное количество элементов для перемещения")
+        return
+
+    print("Элементы успешно перемещены")
+    return
 
 def main():
     while True:
@@ -84,7 +149,7 @@ def main():
         #PARSER SEARCH
         parser_search = subparsers.add_parser('search', help='search help')
         parser_search.add_argument('search', action='store_true')
-        parser_search.add_argument('--name', '-n', dest='name', type=str, help='name help')
+        parser_search.add_argument('--name', '-n', dest='name', type=str, nargs='*', help='name help')
 
         #PARSER STORAGE
         parser_storage = subparsers.add_parser('storage', help='storage help')
@@ -107,12 +172,12 @@ def main():
 
         parser_item.add_argument('--name', '-n', dest='name', type=str)
         parser_item.add_argument('--id', '-i', dest='id', type=int)
-        parser_item.add_argument('--storagename', dest='storagename', type=str)
-        parser_item.add_argument('--storageid', dest='storageid', type=int)
-        parser_item.add_argument('--storagenamesrc', dest='storagenamesrc', type=str)
-        parser_item.add_argument('--storageidsrc', dest='storageidsrc', type=int)
-        parser_item.add_argument('--storagenamedst', dest='storagenamedst', type=str)
-        parser_item.add_argument('--storageiddst', dest='storageiddst', type=int)
+        parser_item.add_argument('--storagename', dest='storageName', type=str)
+        parser_item.add_argument('--storageid', dest='storageId', type=int)
+        parser_item.add_argument('--storagenamesrc', dest='storageNameSrc', type=str)
+        parser_item.add_argument('--storageidsrc', dest='storageIdSrc', type=int)
+        parser_item.add_argument('--storagenamedst', dest='storageNameDst', type=str)
+        parser_item.add_argument('--storageiddst', dest='storageIdDst', type=int)
         parser_item.add_argument('--quantity', '-q', dest='quantity', type=int)
 
         #PARSER TREE
@@ -149,13 +214,18 @@ def main():
                     itemViewByName(args.name)
                 elif args.id is not None:
                     itemViewById(args.id)
-            elif args.create is not None:
+            elif args.create == True:
                 if args.name is not None:
                     itemCreate(args.name)
-            elif args.add is not None:
+            elif args.add == True:
                 if (args.name is not None or args.id is not None) and (args.storageName is not None or args.storageId is not None) and (args.quantity is not None):
                     itemAdd(args.name, args.id, args.storageName, args.storageId, args.quantity)
-
+            elif args.remove == True:
+                if (args.name is not None or args.id is not None) and (args.storageName is not None or args.storageId is not None) and (args.quantity is not None):
+                    itemRemove(args.name, args.id, args.storageName, args.storageId, args.quantity)
+            elif args.move == True:
+                if (args.name is not None or args.id is not None) and (args.storageNameSrc is not None or args.storageIdSrc is not None) and (args.storageNameDst is not None or args.storageIdDst is not None) and (args.quantity is not None):
+                    itemMove(args.name, args.id, args.storageNameSrc, args.storageIdSrc, args.storageNameDst,args.storageIdDst, args.quantity)
 
 
 if __name__ == '__main__':
